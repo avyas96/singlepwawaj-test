4212b9b78430754aad6218670dcc6ef800710f3b
avyas96 <vyaskshay@gmail.com>
2018-07-01 11:15:44 +0530

    Files updated!!

---
+++ app.js
@@ -0,0 +1,23 @@
+//IIFE(IMMEDIATELY INVOKED FUNCTION EXPRESSION)
+(function() {
+  'use strict';
+
+  angular.module('NameCalculatorApp', [])
+  .controller('NameCalculatorController', function($scope) {
+    $scope.name="";
+    $scope.totalValue = 0;
+    $scope.displayNumeric = function(){
+      var totalNameValue = calculateNumericForString($scope.name);
+      $scope.totalValue = totalNameValue;
+    };
+
+    function calculateNumericForString(string) {
+      var totalStringValue = 0;
+      for (var i=0; i< string.length ; i++){
+        totalStringValue += string.charCodeAt(i);
+      }
+      return totalStringValue;
+    }
+  });
+
+}());
